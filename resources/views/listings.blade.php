0565f9c094f527221845585f32614f221cf3c172
Duarti <duartnishefci@gmail.com>
2025-01-22 22:05:22 +0100

    Initial setup

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,14 @@
+<h1>{{$heading}}</h1>
+
+@if(empty($listings))
+<p>No listings found</p>
+@endif
+
+@foreach($listings as $listing)
+    <h2>
+        <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+    </h2>
+    <p>
+        {{$listing['description']}}
+    </p>
+@endforeach
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+
+//All listings
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+//Single listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
