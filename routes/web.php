0b9e86723996d634d4c47cd6b6376e806785697a
Duarti <duartnishefci@gmail.com>
2025-01-23 13:14:33 +0100

    Implement Pagination, Create and Update (ground-up)

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 //All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
+
+//Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store listing
+Route::post('/listings', [ListingController::class, 'store']);
+
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+
+
 
 //Single listing
-Route::get('/listings/{id}', function ($id) {
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
